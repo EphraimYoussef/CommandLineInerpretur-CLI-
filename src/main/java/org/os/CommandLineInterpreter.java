e3efa64777b26252f4e60b591cc2561a47aab540
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2024-10-30 14:10:23 +0300

    revert and solve conflicts

--- src/main/java/org/os/CommandLineInterpreter.java
+++ src/main/java/org/os/CommandLineInterpreter.java
@@ -40,11 +40,8 @@ public class CommandLineInterpreter {
             System.out.println("cd: missing argument");
             return;
         }
-
         // join tokens from index 1 to the end, preserving spaces
         String path = String.join(" ", commandTokens.subList(1, commandTokens.size()));
-
-//        String path = commandTokens.get(1);
         File newDirectory;
 
         //go back one step
@@ -69,37 +66,37 @@ public class CommandLineInterpreter {
 
     //  Lists the contents (files & directories) of the current directory sorted alphabetically
     //except . starters (ls)
-    public void printListFiles(){
+    public void printListFiles() {
         files = currentDirectory.listFiles();
         System.out.println("File names: ");
-        StringBuilder list= new StringBuilder();
+        StringBuilder list = new StringBuilder();
         int count = 1;
         if (files != null) {
             for (File file : files) {
                 // this condition to avoid hiddenfiles
-                if(file.isHidden())
+                if (file.isHidden())
                     continue;
-                if(file.isDirectory())
+                if (file.isDirectory())
                     list.append(count).append("- /").append(file.getName()).append("/, ");
                 else
                     list.append(count).append("-").append(file.getName()).append(", ");
                 count++;
             }
         }
-        System.out.println(list.toString());
+        System.out.println(list);
     }
 
     //List but reversed (ls -r)
     public void printRevListFiles() {
         files = currentDirectory.listFiles();
         System.out.println("File names reversed: ");
-        StringBuilder list= new StringBuilder();
+        StringBuilder list = new StringBuilder();
         int count = 1;
         for (int i = files.length - 1; i >= 0; i--) {
             // this condition to avoid hiddenfiles
-            if(files[i].isHidden())
+            if (files[i].isHidden())
                 continue;
-            if(files[i].isDirectory())
+            if (files[i].isDirectory())
                 list.append(count).append("- /").append(files[i].getName()).append("/, ");
             else
                 list.append(count).append("-").append(files[i].getName()).append(", ");
@@ -110,14 +107,14 @@ public class CommandLineInterpreter {
     }
 
     //  Lists all contents including . starter (ls -a)
-    public void printAllListFiles(){
+    public void printAllListFiles() {
         files = currentDirectory.listFiles();
         System.out.println("File names: ");
-        StringBuilder list= new StringBuilder();
+        StringBuilder list = new StringBuilder();
         int count = 1;
         if (files != null) {
             for (File file : files) {
-                if(file.isDirectory())
+                if (file.isDirectory())
                     list.append(count).append("- /").append(file.getName()).append("/, ");
                 else
                     list.append(count).append("-").append(file.getName()).append(", ");
@@ -127,9 +124,74 @@ public class CommandLineInterpreter {
         System.out.println(list.toString());
     }
 
-    
+    //makedir (mkdir)
+    public void mkdir(List<String> commandTokens) {
+         for (int i =1 ; i< commandTokens.size(); i++) {
+             String input = commandTokens.get(i);
+
+            // Address of current Directory that we will put newdir in
+            String Path = getCurrentDirectory().getAbsolutePath();
 
 
+
+            //created the path of created dir by merging path + \ + name
+            //seprator to make /
+            //makes
+            String DirectoryName_path = Path + File.separator + input;
+
+            File directory = new File(DirectoryName_path);
+
+            //create directory with given path
+            if (directory.mkdirs())
+                System.out.println("Directory Created Succesfully! at : " + DirectoryName_path);
+            else
+                System.out.println("Failed to create Directory ");
+        }
+    }
+
+    public void touch(List<String> commandTokens) throws IOException {
+        for (int i =1 ; i< commandTokens.size(); i++) {
+            //input after touch
+            StringBuilder input = new StringBuilder(commandTokens.get(i));
+
+            //Address of current Directory that we will put newfile in
+            String path = currentDirectory.getAbsolutePath();
+
+            //Handling if file inside dir !
+            String finder = "/";
+
+            //to handle if there is directories
+            int index=input.lastIndexOf(finder);
+
+            if(index != -1)
+                input.setCharAt(index,File.separatorChar);
+
+            //creating the path that we will pass to file function
+            String FileName_path = path + File.separator + input;
+
+            File file = new File(FileName_path);
+
+            if(file.createNewFile())
+                System.out.println("File Created Succesfully! at : " + FileName_path);
+            else
+                System.out.println("Failed to create File ");
+
+
+        }
+    }
+
+    //removedir (rmdir)
+    public void rmdir(){
+        boolean deletionProcess =currentDirectory.delete();
+        if(deletionProcess){
+            System.out.println("Directory Deleted Succesfully!");
+            currentDirectory =currentDirectory.getParentFile();
+        }
+
+        else
+            System.out.println("Failed to Delete Directory (The files isn't Empty)");
+
+    }
 
 
 
