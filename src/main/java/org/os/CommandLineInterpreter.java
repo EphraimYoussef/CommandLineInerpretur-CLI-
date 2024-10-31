a3a2f9d416619a1e76ffe6cf51841eea62265f19
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2024-10-31 17:41:26 +0300

    cat > file , cat >>file

--- src/main/java/org/os/CommandLineInterpreter.java
+++ src/main/java/org/os/CommandLineInterpreter.java
@@ -83,17 +83,17 @@ public class CommandLineInterpreter {
             }
         }
         //case of ls >
-        if(commandTokens.size() == 3 && Objects.equals(commandTokens.get(1), ">")) {
+        if (commandTokens.size() == 3 && Objects.equals(commandTokens.get(1), ">")) {
             //for >
             String toSend = list.toString();
-            greaterThan(toSend,commandTokens.get(2));
+            greaterThan(toSend, commandTokens.get(2));
             return;
         }
         //case of ls >>
-        else if(commandTokens.size() == 3 && Objects.equals(commandTokens.get(1), ">>")) {
+        else if (commandTokens.size() == 3 && Objects.equals(commandTokens.get(1), ">>")) {
             //for >
             String toSend = list.toString();
-            greaterThanThan(toSend,commandTokens.get(2));
+            greaterThanThan(toSend, commandTokens.get(2));
             return;
         }
         System.out.println(list);
@@ -104,7 +104,7 @@ public class CommandLineInterpreter {
         files = currentDirectory.listFiles();
         StringBuilder list = new StringBuilder();
         int count = 1;
-        if(files != null){
+        if (files != null) {
             for (int i = files.length - 1; i >= 0; i--) {
                 // this condition to avoid hiddenFiles
                 if (files[i].isHidden())
@@ -119,17 +119,17 @@ public class CommandLineInterpreter {
         }
 
         //case of ls >
-        if(commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">")) {
+        if (commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">")) {
             //for >
             String toSend = list.toString();
-            greaterThan(toSend,commandTokens.get(3));
+            greaterThan(toSend, commandTokens.get(3));
             return;
         }
         //case of ls >>
-        else if(commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">>")) {
+        else if (commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">>")) {
             //for >
             String toSend = list.toString();
-            greaterThanThan(toSend,commandTokens.get(3));
+            greaterThanThan(toSend, commandTokens.get(3));
             return;
         }
         System.out.println(list.toString());
@@ -150,17 +150,17 @@ public class CommandLineInterpreter {
             }
         }
         //case of ls >
-        if(commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">")) {
+        if (commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">")) {
             //for >
             String toSend = list.toString();
-            greaterThan(toSend,commandTokens.get(3));
+            greaterThan(toSend, commandTokens.get(3));
             return;
         }
         //case of ls >>
-        else if(commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">>")) {
+        else if (commandTokens.size() == 4 && Objects.equals(commandTokens.get(2), ">>")) {
             //for >
             String toSend = list.toString();
-            greaterThanThan(toSend,commandTokens.get(3));
+            greaterThanThan(toSend, commandTokens.get(3));
             return;
         }
         System.out.println(list.toString());
@@ -358,18 +358,49 @@ public class CommandLineInterpreter {
         StringBuilder input = new StringBuilder();
         //path of file before adding name
         input.append(getCurrentDirectory().getAbsolutePath());
-        // case of cat or cat > or cat >>
-        if (commandTokens.size() == 1 || Objects.equals(commandTokens.get(1), ">")|| Objects.equals(commandTokens.get(1), ">>")) {
+
+        // case of cat  alone
+        if (commandTokens.size() == 1)
+        {
             //file name input from user
             Scanner reader = new Scanner(System.in);
             //read user input or check file name beside cat
             input.append(File.separator).append(reader.nextLine()).append(".txt");
         }
         else {
-            //cat file1.txt  / input = file1.txt
-            input.append(File.separator).append(commandTokens.get(1)).append(".txt");
-        }
+            //Here either cat (filename) or cat > filename or cat >>filename
+            String option = commandTokens.get(1);
+             // Handle cat > file , cat >> file case
+            if (Objects.equals(option, ">") || Objects.equals(option, ">>")) {
+                String filename = commandTokens.get(2);
+                StringBuilder userInput = new StringBuilder();   // StringBuilder for user input
 
+                // user input until "stop"
+                Scanner reader = new Scanner(System.in);
+                System.out.println("Enter text (type 'stop' to finish):");
+                while (true) {
+                    String line = reader.nextLine();
+                    if (line.equals("stop")) {
+                        break;
+                    }
+                    userInput.append(line).append(System.lineSeparator());
+                }
+
+                // Redirect to either `greaterThan` or `greaterThanThan` based on the option
+                String content = userInput.toString();
+                if (option.equals(">")) {
+                    greaterThan(content, filename);
+                } else if (option.equals(">>")) {
+                    greaterThanThan(content, filename);
+                }
+                return;  // Exit the method after handling redirection
+
+            }
+            // `cat filename`
+            else {
+                input.append(File.separator).append(option).append(".txt");
+            }
+        }
 
         //convert the path+file name to string
         String filename = input.toString();
@@ -381,7 +412,7 @@ public class CommandLineInterpreter {
             System.out.println("File not found or cannot be read.");
             return;
         }
-        //String Builder for > , >>
+        //String Builder for >
         StringBuilder output = new StringBuilder();
 
         //Scan file content
@@ -392,23 +423,8 @@ public class CommandLineInterpreter {
             System.out.println(line);
         }
         readContent.close();
-
-        //case of cat >
-        if(Objects.equals(commandTokens.get(1), ">")) {
-            //for >
-            String toSend = output.toString();
-            greaterThan(toSend,commandTokens.get(2));
-        }
-        //case of cat >>
-        else if(Objects.equals(commandTokens.get(1), ">>")) {
-            //for >
-            String toSend = output.toString();
-            greaterThanThan(toSend,commandTokens.get(2));
-        }
-
-
-
     }
+
 
     //Redirect output (>)
     public void greaterThan(String output, String filename) throws IOException {
@@ -446,7 +462,7 @@ public class CommandLineInterpreter {
             touch(toSend);
         }
         //path to file
-        FileWriter file = new FileWriter(path.toString(),true);
+        FileWriter file = new FileWriter(path.toString(), true);
         file.write(output);
         file.close();
 
